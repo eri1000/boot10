5770cb835e501cebb337014ec5c6a175f6674267
erfan70 <erfaaaaaaaaan@gmail.com>
2017-07-12 06:08:35 +0430

    boot10

--- bot/bot.lua
+++ bot/bot.lua
@@ -85,7 +85,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
 	config = {
     enabled_plugins = {
-    "banhammer",	
+    "BanHammer",	
     "GroupManager",
     "Msg-Checks",	
     "Plugins",
